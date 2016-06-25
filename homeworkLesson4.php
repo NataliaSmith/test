f46bdd8069b5f69121844fafeff877a7af67b55f
Natalia <kittin967@gmail.com>
2016-06-25 18:57:22 +0300

    *fixed array

--- homeworkLesson4.php
+++ homeworkLesson4.php
@@ -4,16 +4,16 @@ $autoShow = array(
 		'CarBrands'=>"BMW"
 		array(
 			'TheModelOfCar'=>'m5',
-			'YearCar1'=> array('2002','2015')
+			'YearCar'=> array('2002','2015')
 				)
 			)
 	array(
 		'CarBrands'=>"Toyota"
 		array(
 			'TheModelOfCar'=>'Corolla',
-			'YearCar1'=> array('2002','2015')
+			'YearCar'=> array('2002','2015')
 				)
 			'TheModelOfCar'=>'Camry',
-			'YearCar1'=> array('2002','2015')
+			'YearCar'=> array('2002','2015')
 				)
 			);
