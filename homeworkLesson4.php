31c76f7e4787dc45ef68963c2eae5d218b142a28
Natalia <kittin967@gmail.com>
2016-06-28 18:28:59 +0300

    *

--- homeworkLesson4.php
+++ homeworkLesson4.php
@@ -1,10 +1,11 @@
 <?php 
 $autoShowe = array(
-					'bmw'=> array("BMW","m5", 2002, 2015),
-					'Toyota'=>	array('Toyota','Corolla','2002','2015'),
-					'Toyota'=>	array('Toyota','Camry','2002','2015')
-					);
-print_r ($autoShowe);
-echo " $autoShowe ['bmw']  [2] [3] ['Toyota'] [3] [4] "; 
-echo " $autoShowe ['bmw']  [1] ['Toyota'] [1] [2] "; 
-echo " $autoShowe ['bmw']  [0] ['Toyota'] [0] "; 
+		'bmw'=> array("BMW","m5", 2002, 2015),
+		'Toyota'=>	array('Toyota','Corolla','2002','2015'),
+		'Toyota'=>	array('Toyota','Camry','2002','2015')
+		);
+
+	print_r ($autoShowe);
+	echo " $autoShowe ['bmw']  [2] [3] ['Toyota'] [3] [4] "; 
+	echo " $autoShowe ['bmw']  [1] ['Toyota'] [1] [2] "; 
+	echo " $autoShowe ['bmw']  [0] ['Toyota'] [0] "; 
