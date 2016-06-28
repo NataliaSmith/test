488d833a565de4683931727bed95fe6671de3ba6
Natalia <kittin967@gmail.com>
2016-06-28 18:23:00 +0300

    *Home Work

--- homeworkLesson4.php
+++ homeworkLesson4.php
@@ -1,21 +1,10 @@
-<?php
-$autoShow = array(
-	array(
-		'CarBrands'=>"BMW"
-		array(
-			'TheModelOfCar'=>'m5',
-			'YearCar'=> array('2002','2015')
-				)
-			)
-	array(
-		'CarBrands'=>"Toyota"
-		array(
-			'TheModelOfCar'=>'Corolla',
-			'YearCar'=> array('2002','2015')
-				)
-		array(
-			'TheModelOfCar'=>'Camry',
-			'YearCar'=> array('2002','2015')
-				)
-			)
-	);
+<?php 
+$autoShowe = array(
+					'bmw'=> array("BMW","m5", 2002, 2015),
+					'Toyota'=>	array('Toyota','Corolla','2002','2015'),
+					'Toyota'=>	array('Toyota','Camry','2002','2015')
+					);
+print_r ($autoShowe);
+echo " $autoShowe ['bmw']  [2] [3] ['Toyota'] [3] [4] "; 
+echo " $autoShowe ['bmw']  [1] ['Toyota'] [1] [2] "; 
+echo " $autoShowe ['bmw']  [0] ['Toyota'] [0] "; 
